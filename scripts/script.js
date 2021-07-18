148bcaf1a851e7c148476ea14e2ac2057336ce97
Wanderson Vicente <wanderson.vs@outlook.com>
2021-07-18 16:17:22 -0300

    Projeto do Desafio Landpage para Ecommerce da Hiring Coders VTex

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  let data = {
+    name,
+    email,
+  };
+
+  let collectData = JSON.stringify(data);
+  localStorage.setItem("lead", collectData);
+
+  let sendForm = document.getElementById("sendForm");
+
+  let loading = `
+  <p>Estamos salvando seus dados.</p>
+  <p>Por favor aguarde.</P>
+  `;
+
+  let isDone = `
+  <img src="../images/celebrate.gif" width="100%">
+  <P> PARABÉNS!!!</P>
+  <P> Cadrastro realizado com sucesso!!!</P>
+  `;
+
+  sendForm.innerHTML = loading;
+
+  setTimeout(() => {
+    sendForm.innerHTML = isDone;
+  }, 3000);
+});
